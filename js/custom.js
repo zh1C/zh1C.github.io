f8ebcbcdfbde5216b9b96fadc8eb8457d37f9068
narcissus <1044532074@qq.com>
2021-09-04 19:44:04 +0800

    2020-9-4修改博客背景完善

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 /* 轮播背景图片 */
 $(function () {
 	$.backstretch([
-		  "/images/background-img1.png",
-		  "/images/background-img2.png",
+		  "/images/background-img1.jpg",
+		  "/images/background-img2.jpg",
 	], { duration: 60000, fade: 1500 });
 });
