0f60adfea0efb2d24a5b946172efd08b93a5fb78
narcissus <1044532074@qq.com>
2021-09-12 12:33:44 +0800

    2021-9-12背景图片更换

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 /* 轮播背景图片 */
 $(function () {
 	$.backstretch([
-		  "/images/background-img1.jpg",
-		  "/images/background-img2.jpg",
+          "/images/background-img1.jpg",
+          "/images/background-img2.jpg",
           "/images/background-img3.jpg",
           "/images/background-img4.jpg",
 	], { duration: 60000, fade: 1500 });
